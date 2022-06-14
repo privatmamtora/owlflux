4353d7e13a097e9931ec7ed89cf5f7ee16231f03
Krish Mamtora <privat.mamtora@gmail.com>
2022-06-13 23:33:13 -0400

    Implement Required settings for API (save to local storage)

--- src/views/Settings-Core.vue
+++ src/views/Settings-Core.vue
@@ -1,3 +1,31 @@
+<script setup>
+import { ref, watch } from 'vue'
+
+const host = ref('');
+const key = ref('');
+host.value = JSON.parse(localStorage.getItem('host'))
+key.value = JSON.parse(localStorage.getItem('key'))
+
+watch(host, async (newHost) => {
+  localStorage.setItem('host', JSON.stringify(newHost));
+})
+
+watch(key, async (newKey) => {
+  localStorage.setItem('key', JSON.stringify(newKey));
+})
+</script>
+
 <template>
-  Core
+  <v-container>
+    <v-text-field 
+      label="Host"
+      variant="outlined"
+      shaped 
+      v-model="host"></v-text-field>
+    <v-text-field 
+      label="API Key" 
+      variant="outlined"
+      shaped
+      v-model="key"></v-text-field>
+  </v-container>
 </template>
