ccec9a43260755d2592ed8e1b15d119563769942
Krish Mamtora <privat.mamtora@gmail.com>
2022-06-13 23:30:55 -0400

    Move drawer into Home (only accessible from Home)

--- src/App.vue
+++ src/App.vue
@@ -1,73 +1,9 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-
-// let drawer = false;
-// let group = null;
-
-const items = [
-      {
-        title: 'Foo',
-        value: 'foo',
-      },
-      {
-        title: 'Bar',
-        value: 'bar',
-      },
-      {
-        title: 'Fizz',
-        value: 'fizz',
-      },
-      {
-        title: 'Buzz',
-        value: 'buzz',
-      }
-];
-
-// watch(group, async ()=> {this.drawer = false})
-
-</script>
-
-<script>
-  export default {
-    data: () => ({
-      drawer: false,
-      group: null,
-    }),
-
-    watch: {
-      group () {
-        this.drawer = false
-      },
-    },
-  }
+import { RouterView } from 'vue-router'
 </script>
 
 <template>
   <v-app>
-    <v-app-bar color="primary" density="compact">      
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-      <v-toolbar-title>My files</v-toolbar-title>
-    </v-app-bar>
-    <v-navigation-drawer v-model="drawer" color="grey-darken-2" app>
-      <v-list
-        :items="items"
-      ></v-list>
-    </v-navigation-drawer>
-    
-    <!-- <header>
-      <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-      <div class="wrapper">
-        <HelloWorld msg="You did it also!" />
-
-        <nav>
-          <RouterLink to="/">Home</RouterLink>
-          <RouterLink to="/settings">About</RouterLink>
-        </nav>
-      </div>
-    </header> -->
-    <v-main>
-      <RouterView />
-    </v-main>
+    <RouterView />
   </v-app>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,16 +3,68 @@ import TheWelcome from '@/components/TheWelcome.vue'
 import { Splitpanes, Pane } from 'splitpanes'
 import 'splitpanes/dist/splitpanes.css'
 
+const items = [
+      {
+        title: 'Foo',
+        value: 'foo',
+      },
+      {
+        title: 'Bar',
+        value: 'bar',
+      },
+      {
+        title: 'Fizz',
+        value: 'fizz',
+      },
+      {
+        title: 'Buzz',
+        value: 'buzz',
+      }
+];
+
+</script>
+
+<script>
+  export default {
+    data: () => ({
+      drawer: false,
+      group: null,
+    }),
+
+    watch: {
+      group () {
+        this.drawer = false
+      },
+    },
+  }
 </script>
 
 <template>
     <!-- <TheWelcome /> -->
-    <splitpanes class="default-theme">
-      <pane min-size="30">
-        <div>1</div>
-      </pane>
-      <pane min-size="30">
-        <div>2</div>
-      </pane>
-    </splitpanes>
+    <v-navigation-drawer v-model="drawer" app>
+      <v-list-item>
+        <v-list-item-title>OwlFlux</v-list-item-title>
+        <v-btn class="ml-auto" to="/settings">
+        <v-list-item-icon class="ml-auto">mdi-cog</v-list-item-icon>
+      </v-btn>
+      </v-list-item>
+      <v-list
+        :items="items"
+      ></v-list>
+    </v-navigation-drawer>
+    <v-app-bar density="compact">      
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>My files</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <Splitpanes class="default-theme">
+        <Pane min-size="30">
+          <div>1</div>
+        </Pane>
+        <Pane min-size="30">
+          <div>2</div>
+        </Pane>
+      </Splitpanes>
+  </v-main>
 </template>
