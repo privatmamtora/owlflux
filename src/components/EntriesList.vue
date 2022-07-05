ffcea14df1fab2f3efbd171b3fb7843297a362aa
Krish Mamtora <privat.mamtora@gmail.com>
2022-07-04 20:52:24 -0400

    Refactor out EntryList rows & show read/unread status

--- src/components/EntriesList.vue
+++ src/components/EntriesList.vue
@@ -1,12 +1,9 @@
 <script setup>
-import { useTreeStore } from '../stores/tree'
 import { useEntriesStore } from '../stores/entries'
-import { storeToRefs } from 'pinia'
 
-const treeStore = useTreeStore();
+import EntryListItem from "../components/EntryListItem.vue";
+
 const entriesStore = useEntriesStore();
-
-let { iconData } = storeToRefs(treeStore);
 
 const props = defineProps({
   data: {
@@ -16,26 +13,6 @@ const props = defineProps({
 });
 
 console.log('Setup');
-
-function getPreviousDay(date = new Date()) {
-  const previous = new Date(date.getTime());
-  previous.setDate(date.getDate() - 1);
-
-  return previous;
-}
-
-function formatDate(dateString) {
-  let d = new Date(dateString);
-  let min = String(d.getMinutes()).padStart(2, '0');
-  let hours = ("0"+(d.getHours() % 12  || 12)).slice(-2);
-  let ampm = d.getHours() >= 12 ? 'PM' : 'AM';
-
-  if (d > getPreviousDay()) {
-    return `${hours}:${min} ${ampm}`;
-  } else {
-    return `${d.getDate()}/${d.getMonth()}/${d.getFullYear()} ${hours}:${min} ${ampm}`;
-  }
-}
 
 function hasSelected(table) {
   return getSelected(table).length;
@@ -77,21 +54,6 @@ function selectRow(e) {
   console.log(entriesStore.selectedEntry);
 }
 
-function hasIcon(entry) {
-  if(entry.feed.icon && entry.feed.icon.icon_id) {
-    return true;
-     // treeStore.getIconById(feed.icon.icon_id).data
-  }
-  return false;  
-}
-
-function getIcon(entry) {
-  if(entry.feed.icon && entry.feed.icon.icon_id) {
-    return 'data:' + treeStore.getIconById(entry.feed.icon.icon_id).data;
-  }
-  return '';  
-}
-
 </script>
 
 <template>
@@ -99,16 +61,9 @@ function getIcon(entry) {
     density="compact"    
     @click.stop="selectRow">
     <tbody>
-      <tr v-for="entry in props.data.entries"
-        :key="entry.id" 
-        :data-feed-id="entry.id"
-        aria-selected="false">
-        <td v-if="hasIcon(entry)">
-          <img class="icon" :src="getIcon(entry)" />
-        </td>
-        <td>{{ entry.title }}</td>        
-        <td>{{ formatDate(entry.published_at) }}</td>
-      </tr>
+      <template v-for="entry in props.data.entries" :key="entry.id" >
+        <EntryListItem :node="entry" />
+      </template>
     </tbody>
   </v-table>
 </template>
@@ -141,5 +96,9 @@ function getIcon(entry) {
 
 .v-table .v-table__wrapper > table > tbody > tr[aria-selected="true"] {
   background: #ddd;
+}
+
+.v-table .v-table__wrapper > table > tbody > tr {
+  font-weight: bold;
 }
 </style>
---
+++ src/components/EntryListItem.vue
@@ -0,0 +1,70 @@
+<script setup>
+import { ref, computed } from 'vue'
+
+import { useTreeStore } from '../stores/tree';
+
+const treeStore = useTreeStore();
+
+const props = defineProps({
+	node: {
+		type: Object,
+		required: true
+	},
+});
+
+function getPreviousDay(date = new Date()) {
+  const previous = new Date(date.getTime());
+  previous.setDate(date.getDate() - 1);
+
+  return previous;
+}
+
+function formatDate(dateString) {
+  let d = new Date(dateString);
+  if (d > getPreviousDay()) {
+    let min = String(d.getMinutes()).padStart(2, '0');
+    let hours = ("0"+(d.getHours() % 12  || 12)).slice(-2);
+    let ampm = d.getHours() >= 12 ? 'PM' : 'AM';
+    return `${hours}:${min} ${ampm}`;
+  } else {
+    return `${d.getDate()}/${d.getMonth()}/${d.getFullYear()}`;
+  }
+}
+
+function hasIcon(entry) {
+  if(entry.feed.icon && entry.feed.icon.icon_id) {
+    return true;
+     // treeStore.getIconById(feed.icon.icon_id).data
+  }
+  return false;  
+}
+
+function getIcon(entry) {
+  if(entry.feed.icon && entry.feed.icon.icon_id) {
+    return 'data:' + treeStore.getIconById(entry.feed.icon.icon_id).data;
+  }
+  return '';  
+}
+
+const classObject = computed(() => ({
+  active: isActive.value && !error.value,
+  'text-danger': error.value && error.value.type === 'fatal'
+}))
+
+</script>
+<template>
+	<tr :data-feed-id="node.id"
+		aria-selected="false"
+		:class="[{unread: node.status === 'unread'}]">
+		<td v-if="hasIcon(node)">
+			<img class="icon" :src="getIcon(node)" />
+		</td>
+		<td>{{ node.title }}</td>        
+		<td style="width: 0.1%;">
+			<span style="white-space: nowrap;">{{ formatDate(node.published_at) }}</span>
+		</td>
+	</tr>
+</template>
+<style>
+	
+</style>
